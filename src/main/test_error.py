81a027f792920745bd0123df00524aba83ff14f3
lsknawm <lsknawm@gmail.com>
2023-10-09 09:34:10 +0800

    push commit

--- src/main/test_error.py
+++ src/main/test_error.py
@@ -1,7 +1,19 @@
+import json
 import time
+
+import requests
+
 time.sleep(5)
 print('sleep 5s')
 time.sleep(5)
 time.sleep(15)
 print('sleep 45s')
 time.sleep(30)
+dog = input('输入')
+data = {
+    'from': 'en',
+    'to': 'zh',
+    'query': dog
+}
+req = requests.post(url = 'https://fanyi.baidu.com/v2transapi?from=en&to=zh',data=json.dumps(data),headers=aaaaa)
+print(req.text)
--- 身份处理/代理/代理.py
+++ 身份处理/代理/代理.py
@@ -1,6 +1,6 @@
 import requests,urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-proxy = '43.138.20.156:80'      #此处用的代理是有可能用不了的，得多尝试
+proxy = '106.55.181.100:80'      #此处用的代理是有可能用不了的，得多尝试
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
 }
